abfa697aeb240c680a8c418f6e0fc1c35ae19c15
Catherine Landry <86985765+catherinelandry@users.noreply.github.com>
2021-08-26 14:57:37 -0400

    add code files

---
+++ codes/data_prep.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# import all packages
+from nilearn.connectome import ConnectivityMeasure
+from nilearn.input_data import NiftiLabelsMasker
+from load_confounds import Scrubbing
+from nilearn import datasets
+from os.path import join
+
+import nibabel as nib 
+import numpy as np
+import shutil
+import os
+
+# intialize the layout to retrieve the data
+path = '/Users/eliassalimlopez/derivatives/fmriprep/' 
+file_name = 'task-rest_space-MNI152NLin2009cAsym_desc-preproc_bold'
+subjects = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15','16','17', '18']
+condition = ['control', 'deaf']
+task = 'func'
+ext = 'nii.gz'
+
+# variables attribution
+conn_measure = ConnectivityMeasure(kind='correlation', vectorize=True, discard_diagonal=True)              
+all_features = {'condition':[], 'subject':[], 'connectomes':[]} # where all the features are stored
+schaefer_atlas = datasets.fetch_atlas_schaefer_2018(n_rois=100) # load the atlas 
+files_nii = []
+
+for sub in subjects:
+    
+    for cond in condition:
+        filename = f'sub-{cond}{sub}/{task}/sub-{cond}{sub}_{file_name}.{ext}'
+        sub_func = os.path.join(path, filename)
+        # print (sub_func) to keep track of the loop
+
+        if os.path.isfile(sub_func): # verify if path exist 
+            img_load = nib.load(sub_func)
+            files_nii=np.append(files_nii, img_load)
+        
+            confounds = Scrubbing().load(sub_func)
+
+            # initialize the masker
+            masker = NiftiLabelsMasker(labels_img=schaefer_atlas.maps, t_r=2.2, standardize=True,
+                                       verbose= True)
+            masked_data = masker.fit(img_load)
+            timeseries =  masker.transform(img_load, confounds=confounds) 
+
+            correlation_matrix = conn_measure.fit_transform([timeseries])[0]
+
+            # add each subject caracteristics to a container
+            all_features['condition'].append(cond)
+            all_features['subject'].append(sub)
+            all_features['connectomes'].append(correlation_matrix)
+
+np.savez_compressed('schaefern100_features', cond = all_features['condition'], sub = all_features['subject'], 
+                    conn = all_features['connectomes'])
+
+original = r'/Users/eliassalimlopez/PSY6983/schaefern100_features.npz'
+target = r'/Users/eliassalimlopez/PSY6983/conn_matrix/'
+
+shutil.move(original,target) # change the path of the saved data
---
+++ codes/linear_svc.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import ShuffleSplit
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from numpy import load
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+# Define the variables of my model
+data = np.load('schaefern100_features.npz')
+X_features = data['conn'] 
+y_group = data['cond']
+
+# Model fitting 
+X, X_test, y, y_test = train_test_split(
+     X_features,        # x
+     y_group,           # y
+     train_size=0.75,   # 75%/25% split                                                 
+     test_size = 0.25,  
+     shuffle = True,    # shuffle dataset before splitting
+     stratify = y_group, 
+     random_state = 123 # same shuffle every run for reproducibility
+)
+print('Number of participants:\n')
+print('training:', len(X),
+     'testing:', len(X_test))
+print('\n')
+
+cv = ShuffleSplit(n_splits=5, test_size=.3, random_state=0)
+pca = PCA(0.95)
+X = np.vstack(X_features)
+y = y_group = np.array(y_group)
+
+val_accuracies = []
+train_accuracies = []
+model = []
+i=0
+
+# Tuning the hyper-parameters of an estimator for optimization
+for train, val in cv.split(X, y):
+    X_train, X_val = X[train], X[val]
+    y_train, y_val = y[train], y[val]
+    
+    pipe = Pipeline(steps=[('standardscaler', StandardScaler()),
+                           ('feature_selection', pca),('linearsvc', SVC(kernel='linear', C=1))])
+    
+    pipe.fit(X_train, y_train)                  # fit to training data
+    y_val_pred = pipe.predict(X_val)            # classify the condition group using validation set    
+    
+    model.append(pipe['feature_selection'].inverse_transform(pipe['linearsvc'].coef_))
+
+    # get accuracy
+    val_accuracies.append(pipe.score(X_val, y_val)) 
+    train_accuracies.append(pipe.score(X_train, y_train))
+
+    # confusion matrix for all splits
+    val_matrix = confusion_matrix(y_val, y_val_pred, labels=['control', 'deaf'], normalize='true')
+    plt.figure()
+    heatmap = sns.heatmap(val_matrix, vmin = 0, vmax = 1, annot = True, cmap = 'YlGnBu')
+    heatmap.figure.savefig(f'confusion_matrix_{i}.png')
+    i = i+1
+
+# Verify the classification on the test set
+test_accuracies = pipe.score(X_test, y_test) 
+y_test_pred = pipe.predict(X_test)
+
+# Score metrics
+train_accuracies = np.array(train_accuracies)
+val_accuracies = np.array(val_accuracies)
+test_accuracies= np.array(test_accuracies)
+
+print('Accuracy scores (mean ± std) for: \n')
+print(f' - Train set = {train_accuracies.mean()} ± {train_accuracies.std()}')
+print(f' - Validation set = {val_accuracies.mean()} ± {val_accuracies.std()}\n')
+print(f' - Validation set = {test_accuracies.mean()} ± {test_accuracies.std()}')
